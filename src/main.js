50683d416dd3b072946e473a4ced62cb829c8acf
zyg0121 <zhouyiguo2012@qq.com>
2022-07-05 20:09:31 +0800

    add vue-axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import './plugins/element.js'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+axios.defaults.withCredentials = true
 
 new Vue({
   render: h => h(App),
