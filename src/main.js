fd741034285c6b48f7b072c867447c13a360de56
zyg0121 <zhouyiguo2012@qq.com>
2022-07-05 22:20:30 +0800

    add vue-router 同时增加前后端交互示范

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import './plugins/axios'
 import App from './App.vue'
 import './plugins/element.js'
 import axios from 'axios'
+import router from './router'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 axios.defaults.withCredentials = true
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
