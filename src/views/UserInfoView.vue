324b72993b107c1974602d828a5c9dc5f93143be
violetplus <wangkai_violet@qq.com>
2022-07-06 19:40:08 +0800

    Create UserInfoView.vue

---
+++ src/views/UserInfoView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="userInfo">
+		上传测试
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "userInfo",
+		components:{
+			
+		},
+		data() {
+			return{
+				goodsList:[]
+			}
+		},
+		created() {
+			
+		},
+		methods:{
+			
+		}
+
+
+    }
+
+</script>
+
+<style scoped>
+
+</style>
